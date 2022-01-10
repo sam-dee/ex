d39025c88b0bf74d4635adeee9507c2a3754bafa
sam-dee <mishat.tarasov2001@gmail.com>
2022-01-10 10:53:34 +0300

    Added implicit function, event and made final cleanup

--- src/desmos/ui/MainFrame.kt
+++ src/desmos/ui/MainFrame.kt
@@ -27,16 +27,19 @@ class MainFrame : JFrame() {
                 yMin.value as Double, yMax.value as Double,
             )
 
-            val polyPainter = PolyPainterContainer(plane)
-            polyPainter.pointColor = cjp1.background
-            polyPainter.polyColor = cjp2.background
-            polyPainter.derivColor = cjp3.background
+            val explicitfunctionPainter = FunctionPainter(plane)
+            explicitfunctionPainter.function = { x: Double -> 1 + (x + 2) * (x + 2) }
 
-            polyPainter.paint_points = cb1.isSelected
-            polyPainter.paint_poly = cb2.isSelected
-            polyPainter.paint_deriv = cb3.isSelected
+            val implicitfunctionPainter = ImplicitFunctionPainter(
+                plane,
+                tMin.value as Double,
+                tMax.value as Double
+            )
+            implicitfunctionPainter.function_x = { t: Double -> asin(sin(t)) }
+            implicitfunctionPainter.function_y = { t: Double -> acos(cos(t)) }
 
-            val painters = mutableListOf(CartesianPainter(plane), polyPainter)
+
+            val painters = mutableListOf(CartesianPainter(plane), explicitfunctionPainter, implicitfunctionPainter)
 
             mainPanel = GraphicsPanel(painters).apply {
                 background = Color.WHITE
@@ -50,30 +53,14 @@ class MainFrame : JFrame() {
                 }
             })
 
-//            mainPanel.addMouseListener(object : MouseAdapter() {
-//                override fun mouseClicked(e: MouseEvent?) {
-//                    with(polyPainter) {
-//                            if (e != null) {
-//                                addPoint(plane.xScr2Crt(e.x) to plane.yScr2Crt(e.y))
-//                                mainPanel.repaint()
-//                            }
-//                    }
-//                }
-//            })
-
-//            mainPanel.addMouseWheelListener {
-//                println(it)
-//            }
-
 //            colors mapping
             listOf(cjp1, cjp2).forEach {
                 with(it) {
                     addMouseListener(object : MouseAdapter() {
                         override fun mouseClicked(e: MouseEvent?) {
                             background = JColorChooser.showDialog(mainPanel, "Выберите цвет", Color.RED)
-                            if (it == cjp1) polyPainter.pointColor = background
-                            if (it == cjp2) polyPainter.polyColor = background
-                            if (it == cjp3) polyPainter.derivColor = background
+                            if (it == cjp1) explicitfunctionPainter.funColor = background
+                            if (it == cjp2) implicitfunctionPainter.funColor = background
                             mainPanel.repaint()
                         }
                     })
@@ -95,11 +82,10 @@ class MainFrame : JFrame() {
 
             cb2.addMouseListener(object : MouseAdapter() {
                 override fun mouseClicked(e: MouseEvent?) {
-                    print(cb2.isSelected)
                     if (cb2.isSelected) {
-                        painters.add(explicitfunctionPainter)
+                        painters.add(implicitfunctionPainter)
                     } else {
-                        painters.remove(explicitfunctionPainter)
+                        painters.remove(implicitfunctionPainter)
                     }
                     painters[0] // threads?..
                     mainPanel.repaint()
@@ -124,6 +110,16 @@ class MainFrame : JFrame() {
             yMax.addChangeListener {
                 yMinM.maximum = yMax.value as Double - 0.1
                 plane.ySegment = Pair(yMin.value as Double, yMax.value as Double)
+                mainPanel.repaint()
+            }
+            tMin.addChangeListener {
+                tMinM.minimum = tMin.value as Double - 0.1
+                implicitfunctionPainter.t_min = tMin.value as Double
+                mainPanel.repaint()
+            }
+            tMax.addChangeListener {
+                tMaxM.maximum = tMax.value as Double + 0.1
+                implicitfunctionPainter.t_max = tMax.value as Double
                 mainPanel.repaint()
             }
             plane.width = mainPanel.width
